2c2d4e1dc297d10b8b7ff9fa601288a4209da244
Dayoung <rlfekdud1139@naver.com>
2021-05-11 19:21:00 +0900

    21.05.11

--- Programmers/코딩테스트 연습/Level 2/최댓값과 최솟값.cpp
+++ Programmers/코딩테스트 연습/Level 2/최댓값과 최솟값.cpp
@@ -1,33 +1,49 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <cmath>
 #include <algorithm>
-
 using namespace std;
 
 string solution(string s) {
     string answer = "";
-    vector<int> num;
+
+    vector<int> number;
     int i = 0;
     while (1) {
-        if (s[i] == '-') {
+        vector<int> tmp;
+        int num = 0;
+        while (1) {
+            if (s[i] == '-') {
+                num = -1;
+            }
+            else if (s[i] >= '0' && s[i] <= '9') {
+                if (num == -1) {
+                    tmp.push_back((s[i] - 48) * (-1));
+                }
+                else {
+                    tmp.push_back(s[i] - 48);
+                }
+            }
+            else {
+                break;
+            }
             i++;
-            num.push_back(int(s[i]) * (-1));
         }
-        else if (s[i] != ' ') {
-            num.push_back(int(s[i]));
+        int n = 0;
+        for (int i = 0; i < tmp.size(); i++) {
+            n += (tmp[i] * pow(10, tmp.size() - 1 - i));
         }
+        number.push_back(n);
         i++;
         if (i >= s.size()) {
             break;
         }
     }
+    sort(number.begin(), number.end());
+    answer += to_string(number[0]);
+    answer += ' ';
+    answer += to_string(number.back());
 
-    sort(num.begin(), num.end());
-    for (int i = 0; i < num.size() - 1; i++) {
-        answer += to_string(num[i]);
-        answer += ' ';
-    }
-    answer += to_string(num[num.size() - 1]);
+
     return answer;
 }
