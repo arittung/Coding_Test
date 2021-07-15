d6b0290b2d74d1ce685429266273de8ddd30de01
Dayoung <rlfekdud1139@naver.com>
2021-07-15 11:18:43 +0900

    21.07.15

---
+++ Baekjoon/5565.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int total;
+	cin >> total;
+	for (int i = 0; i < 9; i++) {
+		int a;
+		cin >> a;
+		total -= a;
+	}
+	cout << total;
+
+	
+
+	return 0;
+}
