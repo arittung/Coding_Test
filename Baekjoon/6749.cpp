973c8b1b210769cb18a0d965000a80c16a80bb40
Dayoung <rlfekdud1139@naver.com>
2021-07-30 20:55:07 +0900

    21.07.30

---
+++ Baekjoon/6749.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int first, second, third;
+	cin >> third >> second;
+
+	first = second + second - third;
+	cout << first << endl;
+
+	return 0;
+}
