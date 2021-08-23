e91d741548689df0b6643221467fa6087183d365
Dayoung <rlfekdud1139@naver.com>
2021-08-23 23:17:24 +0900

    21.08.23

---
+++ Baekjoon/19944.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	if (m == 1 || m == 2) {
+		cout << "NEWBIE!" << endl;
+	}
+	else {
+		if (m <= n) {
+			cout << "OLDBIE!" << endl;
+		}
+		else {
+			cout << "TLE!" << endl;
+		}
+	}
+	return 0;
+}
